7b7cd35010135d5ffb49a1e5a2ac32864933b233
Daniil Golovats <daniil.golovats@tktk.ee>
2024-03-06 22:56:13 +0200

    add week4loops code

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿Random rnd = new Random();
+
+int myRandomNUm;
+
+int randomSum = 0;
+
+for(int i = 0; i < 3; i++)
+{
+    myRandomNUm = rnd.Next(0, 11); // 0 - 10
+    randomSum = randomSum + myRandomNUm;
+    Console.WriteLine($"My random number is: {myRandomNUm}");
+}
+
+Console.WriteLine($"random sum total: {randomSum}");
---
+++ Week4Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿
+//FOR-LOOP
+//DRY-principle - Do Not Repeat Yourself
+
+
+for(int i = 0; i < 5; i++ ) //i++ = i + 1
+{
+    Console.WriteLine("We do not need no education");
+}
+
+Console.WriteLine("i"); 
+Console.WriteLine("Have a nice day.");
+
+//Console.WriteLine("We do not need no education")
+//Console.WriteLine("We do not need no education")
+//Console.WriteLine("We do not need no education")
+//Console.WriteLine("We do not need no education")
